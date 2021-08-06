981c2f587f3798ca367f57ef4c0b69910fb3b4bd
emilyliu0410 <39753915+emilyliu0410@users.noreply.github.com>
2021-08-06 14:21:47 +0800

    ui test

--- src/app/form-calendar/form-calendar/form-calendar.component.ts
+++ src/app/form-calendar/form-calendar/form-calendar.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { CalendarModal,CalendarComponentMonthChange, CalendarComponentOptions, CalendarModalOptions } from 'ion2-calendar'
+import { ElementRef, ViewChild } from '@angular/core';
+
 @Component({
   selector: 'app-form-calendar',
   templateUrl: './form-calendar.component.html',
   styleUrls: ['./form-calendar.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
+
 export class FormCalendarComponent  {
   date: Date = new Date();
   // dateMulti: string[];
   // type: 'string';
   // cssClass: 'string';
+  
+
+ 
+    
+ 
   options: CalendarComponentOptions = {
     monthFormat: 'YYYY年MM月',
     monthPickerFormat: ['1','2','3','4' ,'5','6','7','8','9','10','11','12'],
@@ -20,22 +28,11 @@ export class FormCalendarComponent  {
   showMonthPicker:false
   
     }
-
-  // optionsMulti: CalendarComponentOptions = {
-  //   pickMode: 'multi'
-  // };
-  constructor() { }
-  // options: CalendarModalOptions = {
-  //   cssClass: 'my-class',
-  //   title: 'CUSTOM-STYLE',
-  //   // color:'danger',
-  //   defaultDate: this.date,
-  //   monthFormat: 'YYYY年MM月',
-  // //   monthPickerFormat: ['1','2','3','4' ,'5','6','7','8','9','10','11','12'],
-  //   weekdays: ['日','一','二','三','四','五','六'],
-  // showToggleButtons:true
-    
-  // };
+    // @ViewChild('mylbl') mylblRef: ElementRef;
   
+  constructor() { 
+    // console.log(this.mylblRef.nativeElement.innerText);
+  }
+
 
 }
